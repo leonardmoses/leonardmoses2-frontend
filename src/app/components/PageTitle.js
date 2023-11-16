5e959acece0fac5625e7adc738491f997c087287
Qalexeon <leonardsamuelmoses@gmail.com>
2023-11-15 21:51:14 -0500

    Mobile Responsivness. Responsivness Me page, Nav links, Resume. Resume button style. Updated resume png Added new @media Res of 600.

--- src/app/components/PageTitle.js
+++ src/app/components/PageTitle.js
@@ -3,7 +3,7 @@ import styles from "./PageTItle.module.scss"
 const PageTitle = (props) => {
   return (
     <div className={styles.TitleDiv}>
-      <h2>{props.title}</h2>
+      <h2 className={styles.h2}>{props.title}</h2>
     </div>
   );
 };
--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,18 +1,34 @@
 import PageTitle from "../components/PageTitle";
 import styles from "./Resume.module.scss";
+import Image from "next/image";
+import resume from "../static/Moses_Resume_Software.png";
+
 
 const Resume = () => {
   return (
     <main>
-
       <head>
         <title>Resume</title>
       </head>
 
       <div className="MainContent">
-        <PageTitle title="Resume"/>
+        <PageTitle title="Resume" />
+
+        <div className={styles.DownloadDiv}>
+          <h3 className={styles.h3}>Updated October 2023</h3>
+          <a
+            href="../static/Moses_Resume_Software.pdf"
+            download
+            className={styles.ResumeButton}>
+            Download Resume
+          </a>
+        </div>
+
+        <div className={styles.ResumeImageDiv}>
+          <Image className={styles.ResumeImage} src={resume} alt="Resume" />
+        </div>
+
       </div>
-      
     </main>
   );
 };
