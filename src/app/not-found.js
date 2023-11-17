5728aa095ee76ef46973f6d313c25a9e338f20e9
Qalexeon <leonardsamuelmoses@gmail.com>
2023-11-17 11:40:53 -0500

    Added non-found.js for 404 error.

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import PageTitle from "./components/PageTitle";
+
+const NotFoundPage = () => {
+  return (
+    <main>
+      <PageTitle title="Page Not Found" />
+    </main>
+  );
+};
+
+export default NotFoundPage;
