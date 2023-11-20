0a7535d1207e727108b8d8fd19ab6f4d8126b178
Qalexeon <leonardsamuelmoses@gmail.com>
2023-11-20 14:08:03 -0500

    Added Diy Content and styling.

--- src/app/diy/page.js
+++ src/app/diy/page.js
@@ -2,6 +2,17 @@ import PageTitle from "../components/PageTitle";
 import styles from "./Diy.module.scss";
 
 const Diy = () => {
+  const diyDb = [
+    {
+      name: "WallmountFront",
+      image: "https://i.imgur.com/ZCMmiN8.jpg",
+    },
+    {
+      name: "WallmountSide",
+      image: "https://i.imgur.com/Or35GGR.jpg",
+    },
+  ];
+
   return (
     <main>
       <head>
@@ -37,64 +48,72 @@ const Diy = () => {
 
         <div className={styles.Wallmount}>
           <h3>Wallmount</h3>
+
           <div className={styles.WallmountWriteUp}>
-            <p>
-              Meet Magneto, my primary desktop computer. It is computer with all
-              its parts wall-mounted onto a frame. To me, this piece of
-              technology, is also a piece of art. It is where I married my DIY
-              and IT Skills.
-            </p>
-            <p>
-              It all started when I had an iMac but wasn’t satisfied with its
-              graphics capabilities. After researching, I came upon three
-              solutions.{" "}
-            </p>
-            <p>
-              The first was to use an e-GPU, where you connect a more powerful
-              graphics card externally using a thunderbolt cable (Thunderbolt 1
-              at the time). The main problem with that was thunderbolt speeds
-              were much slower than the new graphics card could provide, only
-              giving 30-50% of the performance.
-            </p>
-            <p>
-              The second solution, which would give me 100% of the performance,
-              was to simply use a PC instead. This includes buying PC parts,
-              installing windows, and using windows as my primary operating
-              system. However, this was not an option for me as I wanted to
-              still use macOS.{" "}
-            </p>
+            <div className={styles.WallmountWriteUpLeft}>
+              <p>
+                Meet Magneto, my primary desktop computer. It is computer with
+                all its parts wall-mounted onto a frame. To me, this piece of
+                technology, is also a piece of art. It is where I married my DIY
+                and IT Skills.
+              </p>
+              <p>
+                It all started when I had an iMac but wasn’t satisfied with its
+                graphics capabilities. After researching, I came upon three
+                solutions.
+              </p>
+              <p>
+                The first was to use an e-GPU, where you connect a more powerful
+                graphics card externally using a thunderbolt cable (Thunderbolt
+                1 at the time). The main problem with that was thunderbolt
+                speeds were much slower than the new graphics card could
+                provide, only giving 30-50% of the performance.
+              </p>
+              <p>
+                The second solution, which would give me 100% of the
+                performance, was to simply use a PC instead. This includes
+                buying PC parts, installing windows, and using windows as my
+                primary operating system. However, this was not an option for me
+                as I wanted to still use macOS.
+              </p>
+              <p>
+                The third solution was inspired from a question I had. Apple
+                made it possible to install windows on mac so why not the
+                opposite, installing mac on windows. After all, they both used
+                intel processors. I discovered it was possible! Some people call
+                it hackintoshing and it allows you to install macOS on PC parts.
+              </p>
+            </div>
 
-            <p>
-              The third solution was inspired from a question I had. Apple made
-              it possible to install windows on mac so why not the opposite,
-              installing mac on windows. After all, they both used intel
-              processors. I discovered it was possible! Some people call it
-              hackintoshing and it allows you to install macOS on PC parts.{" "}
-            </p>
+            <div className={styles.WallmountWriteUpRight}>
+              <p>
+                I built the frame out of wood and vinyl wrapped it for a clean
+                finish. I measured and cut out the appropriate holes for running
+                the wiring and created raised transparent platforms out of
+                acrylic sheets. I ordered all the computer parts and wiring
+                necessary. I also ran some LED lighting behind the clear acrylic
+                sheets for mood lighting.
+              </p>
 
-            <p>
-              I built the frame out of wood and vinyl wrapped it for a clean
-              finish. I measured and cut out the appropriate holes for running
-              the wiring and created raised transparent platforms out of acrylic
-              sheets. I ordered all the computer parts and wiring necessary. I
-              also ran some LED lighting behind the clear acrylic sheets for
-              mood lighting.{" "}
-            </p>
+              <p>
+                After all the hardware was completed installing macOS did not go
+                as expected. There was a lot to learn and felt like a foreign
+                language to me. However, I did not give up. I already invested a
+                few thousand dollars into these parts as well as the time it
+                took to build it. I reminded myself that I am a problem solver
+                and have always been able to figure out a solution to every
+                challenge. I continued on for days learning and researching what
+                I needed to and after many failed attempted, I finally saw the
+                apple logo boot up and I managed to install macOS! Not only
+                could I install macOS but I could also install Windows and Linux
+                each on separate drives. It was an incredible victory and there
+                was no going back after that!
+              </p>
+            </div>
 
-            <p>
-              After all the hardware was completed installing macOS did not go
-              as expected. There was a lot to learn and felt like a foreign
-              language to me. However, I did not give up. I already invested a
-              few thousand dollars into these parts as well as the time it took
-              to build it. I reminded myself that I am a problem solver and have
-              always been able to figure out a solution to every challenge. I
-              continued on for days learning and researching what I needed to
-              and after many failed attempted, I finally saw the apple logo boot
-              up and I managed to install macOS! Not only could I install macOS
-              but I could also install Windows and Linux each on separate
-              drives. It was an incredible victory and there was no going back
-              after that!
-            </p>
+            <div className={styles.WallmountEachImage}>
+              <img src={diyDb[0].image} alt={diyDb.name} />
+            </div>
           </div>
         </div>
       </div>
