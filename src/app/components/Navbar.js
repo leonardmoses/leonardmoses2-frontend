afdbfdcd3b065671663de350c6322980dddfe0fa
Qalexeon <leonardsamuelmoses@gmail.com>
2023-11-22 16:12:55 -0500

    Modified Hover Links and hover logo.

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+ import Link from "next/link";
 import Image from "next/image"
 import Logo from "../static/Logo.png"
 
@@ -18,12 +18,12 @@ const Navbar = () => {
             </div>
             
             <ul className={styles.NavLinks}>
-                <li><Link href="/me">Me</Link></li>
-                <li><Link href="/portfolio">Portfolio</Link></li>
-                <li><Link href="/resume">Resume</Link></li>
-                <li><Link href="/art">Art</Link></li>
-                <li><Link href="/music">Music</Link></li>
-                <li><Link href="/diy">Diy</Link></li>
+                <li ><Link className={styles.LinkMe} href="/me">Me</Link></li>
+                <li ><Link className={styles.LinkPortfolio} href="/portfolio">Portfolio</Link></li>
+                <li ><Link className={styles.LinkResume} href="/resume">Resume</Link></li>
+                <li ><Link className={styles.LinkArt} href="/art">Art</Link></li>
+                <li ><Link className={styles.LinkMusic} href="/music">Music</Link></li>
+                <li ><Link className={styles.LinkDiy} href="/diy">Diy</Link></li>
             </ul>
             <div className={styles.NavBottomBorder}></div>
         </main>
